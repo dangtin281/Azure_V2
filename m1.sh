f4c40a754108d39158aca623b72fed220cef73b1
dangtin281 <86880836+dangtin281@users.noreply.github.com>
2022-03-31 10:40:57 +0700

    Update m1.sh

--- m1.sh
+++ m1.sh
@@ -2,6 +2,7 @@
 
 echo "dangtin281/Azure_V2" > gitpath.txt
 gitpath=$(head -1 gitpath.txt)
+whoami > inuser.txt
 
 wget https://raw.githubusercontent.com/$gitpath/main/ubuntu-driver-ins.sh
 chmod +x ubuntu-driver-ins.sh
